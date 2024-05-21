bf22b41945b9d564ca904a7c0c3d931826812a5d
semjasa83 <marceldezanna@yahoo.de>
2024-05-22 00:47:37 +0200

    contacts sorted

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './main/home/content/dashboard/dashboard.comp
 import { ContactsComponent } from './main/home/content/contacts/contacts.component';
 import { AddTaskComponent } from './main/home/content/add-task/add-task.component';
 import { BoardComponent } from './main/home/content/board/board.component';
+import { ContactDetailComponent } from './main/home/content/contacts/contact-detail/contact-detail.component';
 
 
 export const routes: Routes = [
@@ -14,7 +15,12 @@ export const routes: Routes = [
       { path: 'dashboard', component: DashboardComponent, data: { name: 'Dashboard', icon: 'space_dashboard' } },
       { path: 'addtask', component: AddTaskComponent, data: { name: 'Add Task', icon: 'edit_square' } },
       { path: 'board', component: BoardComponent, data: { name: 'Board', icon: 'calendar_view_week' } },
-      { path: 'contacts', component: ContactsComponent, data: { name: 'Contacts', icon: 'perm_contact_calendar' } },
+      { path: 'contacts', component: ContactsComponent, data: { name: 'Contacts', icon: 'perm_contact_calendar' },
+          children: [
+            { path: 'contact-detail/:id', component: ContactDetailComponent }
+          ]
+          
+        },
       // { path: '**', component: DashboardComponent },
     ],
   },
--- src/app/main/home/content/contacts/contacts.component.ts
+++ src/app/main/home/content/contacts/contacts.component.ts
@@ -3,17 +3,23 @@ import { ContactsService } from '../../../../services/contacts/contacts.service'
 import { Contact } from "../../../../interfaces/contact";
 import { ButtonComponent } from '../../../utility/button/button.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterOutlet } from '@angular/router';
+import { JsonPipe, NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
   imports: [
     TranslateModule,
-    ButtonComponent
+    ButtonComponent,
+    RouterOutlet,
+    NgFor,
+    JsonPipe
   ],
   providers: [
     ContactsService,
-
+    { provide: 'Object', useValue: Object }
   ],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
@@ -23,30 +29,78 @@ export class ContactsComponent {
 
 
   public contacts: Contact[] = [];
+  public groupContacts: { [key: string]: Contact[] } = {};
+  // public groupedContactsArray = Object.entries(this.groupContacts);
+  public groupLetters: string[] = [];
+  public groupedContactsArray: Contact[] = [];
+  public groupArray: any[] = [];
 
   constructor(public contactsService: ContactsService) { }
 
   public ngOnInit() {
     this.getContactList();
 
-  }
+  };
 
-  public async getContactList() {
+  public async getContactList(): Promise<void> {
     this.contacts = [];
     this.contactsService.getAllContacts().subscribe(data => {
-      this.sortedContacts(data);
+      this.sortContacts(data);
     });
-  }
+  };
 
-  public async sortedContacts(data: any) {
+  public async sortContacts(data: any): Promise<void> {
     const arr = Object.keys(data);
-    let key = arr[0];
+    const key = arr[0];
     this.contacts = data[key];
     this.contacts?.sort((a, b) => {
       return a.lastName.localeCompare(b.lastName);
-    });
-    console.log(this.contacts);
+    })
+    //console.log(this.contacts); //_CONSOLE
+    this.getGroupedContacts();
   };
 
 
+  public async getGroupedContacts(): Promise<void> {
+    this.groupContacts = {};
+    // this.groupedContactsArray = [];
+    this.contacts.forEach((contact) => {
+      const letter = contact.lastName.charAt(0).toUpperCase();
+      if (!this.groupContacts[letter]) {
+        this.groupContacts[letter] = [];
+      }
+      this.groupContacts[letter].push(contact);
+    });
+    //console.log(this.groupContacts); //_CONSOLE
+    this.test();
+  };
+
+
+  public async test() {
+    let render = document.getElementById('contactList')
+    const arr = Object.entries(this.groupContacts);
+    console.log(arr);
+    arr.forEach((group) => {  
+
+      this.groupLetters.push(group[0]);
+      //console.log(this.groupLetters);
+      
+      for (let i = 0; i < group[1].length; i++) {
+        const c = group[1][i];
+        this.groupedContactsArray.push(c);
+      }
+      this.groupArray.push(this.groupedContactsArray);
+      console.log(this.groupedContactsArray);
+      this.groupedContactsArray = [];
+    }
+    );
+
+    console.log(this.groupArray);
+    
+    // if (render) {
+    // render.innerHTML += /*html*/`
+    // <div>{{}}</div>
+    // `;
+    // }
+}
 }
