75d84ce4f5cc6c85d6b8bc36358a0a53b923d097
semjasa83 <marceldezanna@yahoo.de>
2024-09-02 18:01:02 +0200

    start building tasks

--- src/app/main/home/content/add-task/add-task.component.ts
+++ src/app/main/home/content/add-task/add-task.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { InputFieldComponent } from "../../../utility/input-field/input-field.component";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
   imports: [
-    InputFieldComponent
+    InputFieldComponent,
+    TranslateModule
   ],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss'
